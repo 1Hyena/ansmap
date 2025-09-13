e970cea8ead5a4d144d049631854b97d38f31f3e
1Hyena <mail@ericherstu.net>
2025-09-13 20:55:12 +0300

    added utility functions

--- ansmap.php
+++ ansmap.php
@@ -28,7 +28,7 @@ function ansmap_create($width, $height) {
         $row = array();
 
         for ($j = 0; $j < $width; ++$j) {
-            $row[] = ansmap_create_cell();
+            $row[] = ansmap_cell_create();
         }
 
         $ansmap[] = $row;
@@ -37,16 +37,29 @@ function ansmap_create($width, $height) {
     return $ansmap;
 }
 
-function ansmap_create_cell() {
+function ansmap_get_height(&$amp) {
+    return count($amp);
+}
+
+function ansmap_get_width(&$amp) {
+    return ansmap_get_height($amp) > 0 ? count($amp[0]) : 0;
+}
+
+function ansmap_cell_create() {
     return array(
-        "symbol" => " "
+        "symbol"        => " ",
+        "background"    => " ",
+        "foreground"    => " ",
+        "style"         => " "
     );
 }
 
-function ansmap_clone_cell($cell) {
-    $clone = ansmap_create_cell();
+function ansmap_cell_clone($cell) {
+    $clone = ansmap_cell_create();
 
-    $clone["symbol"] = $cell["symbol"];
+    foreach ($cell as $key => $value) {
+        $clone[$key] = $value;
+    }
 
     return $clone;
 }
@@ -61,6 +74,8 @@ function ansmap_blit(
     // clipping rectangle, and it will also clip if you try to blit from areas
     // outside the source bitmap.
 
+    $buffer = array();
+
     for ($dy = 0; $dy < $height; ++$dy) {
         if ($src_y + $dy >= count($src) || $dst_y + $dy >= count($dst)) {
             break;
@@ -72,21 +87,101 @@ function ansmap_blit(
                 break;
             }
 
-            $dst[$dst_y + $dy][$dst_x + $dx] = ansmap_clone_cell(
+            $buffer[] = ansmap_cell_clone(
                 $src[$src_y + $dy][$src_x + $dx]
             );
         }
     }
+
+    $i = 0;
+
+    for ($dy = 0; $dy < $height; ++$dy) {
+        if ($src_y + $dy >= count($src) || $dst_y + $dy >= count($dst)) {
+            break;
+        }
+
+        for ($dx = 0; $dx < $width; ++$dx) {
+            if ($src_x + $dx >= count($src[$src_y + $dy])
+            ||  $dst_x + $dx >= count($dst[$dst_y + $dy])) {
+                break;
+            }
+
+            $dst[$dst_y + $dy][$dst_x + $dx] = $buffer[$i++];
+        }
+    }
 }
 
-function ansmap_draw_symbol(&$amp, $sym, $x, $y) {
-    $h = count($amp);
+function ansmap_cell_draw(&$dst, &$src) {
+    foreach ($src as $key => $value) {
+        if ($value === " ") {
+            continue;
+        }
 
-    if ($y < 0 || $y >= $h || $x < 0 || $x >= count($amp[$y])) {
+        $dst[$key] = $value;
+    }
+}
+
+function ansmap_draw_sprite(&$dst, &$src, $x, $y) {
+    // Draws a copy of the sprite ansmap onto the destination ansmap at the
+    // specified position. This is almost the same as blit(sprite, amp, 0, 0, x,
+    // y, sprite->w, sprite->h), but it uses a masked drawing mode where
+    // transparent values are skipped, so the background image will show through
+    // the masked parts of the sprite. Transparent values are marked by a space
+    // symbol.
+
+    $width = ansmap_get_width($src);
+    $height= ansmap_get_height($src);
+    $dst_x = $x;
+    $dst_y = $y;
+    $src_x = 0;
+    $src_y = 0;
+
+    $buffer = array();
+
+    for ($dy = 0; $dy < $height; ++$dy) {
+        if ($src_y + $dy >= count($src) || $dst_y + $dy >= count($dst)) {
+            break;
+        }
+
+        for ($dx = 0; $dx < $width; ++$dx) {
+            if ($src_x + $dx >= count($src[$src_y + $dy])
+            ||  $dst_x + $dx >= count($dst[$dst_y + $dy])) {
+                break;
+            }
+
+            $buffer[] = ansmap_cell_clone(
+                $src[$src_y + $dy][$src_x + $dx]
+            );
+        }
+    }
+
+    $i = 0;
+
+    for ($dy = 0; $dy < $height; ++$dy) {
+        if ($src_y + $dy >= count($src) || $dst_y + $dy >= count($dst)) {
+            break;
+        }
+
+        for ($dx = 0; $dx < $width; ++$dx) {
+            if ($src_x + $dx >= count($src[$src_y + $dy])
+            ||  $dst_x + $dx >= count($dst[$dst_y + $dy])) {
+                break;
+            }
+
+            ansmap_cell_draw($dst[$dst_y + $dy][$dst_x + $dx], $buffer[$i++]);
+        }
+    }
+}
+
+function ansmap_channel_set_value(&$amp, $channel, $value, $x, $y) {
+    $h = ansmap_get_height($amp);
+    $w = ansmap_get_width($amp);
+
+    if ($y < 0 || $y >= $h || $x < 0 || $x >= $w) {
         return;
     }
 
-    $amp[$y][$x]["symbol"] = $sym;
+    $amp[$y][$x][$channel] = $value;
 }
 
 function ansmap_draw_text(&$amp, $txt, $x, $y) {
@@ -98,7 +193,7 @@ function ansmap_draw_text(&$amp, $txt, $x, $y) {
         $sym_x = $x;
 
         foreach ($symbols as $sym) {
-            ansmap_draw_symbol($amp, $sym, $sym_x, $sym_y);
+            ansmap_channel_set_value($amp, "symbol", $sym, $sym_x, $sym_y);
             $sym_x++;
         }
 
@@ -106,7 +201,7 @@ function ansmap_draw_text(&$amp, $txt, $x, $y) {
     }
 }
 
-function ansmap_create_from_text($txt) {
+function ansmap_create_from_text($txt, $channel = "symbol") {
     $lines = explode("\n", $txt);
     $sym_y = 0;
 
@@ -138,18 +233,51 @@ function ansmap_create_from_text($txt) {
 
     for ($y = 0; $y < count($rows); ++$y) {
         for ($x = 0; $x < count($rows[$y]); ++$x) {
-            ansmap_draw_symbol($amp, $rows[$y][$x], $x, $y);
+            ansmap_channel_set_value($amp, $channel, $rows[$y][$x], $x, $y);
         }
     }
 
     return $amp;
 }
 
+function ansmap_create_from_texts(
+    $background_colors, $symbols = "", $foreground_colors = "", $style = ""
+) {
+    $amp_bgc = ansmap_create_from_text($background_colors,  "background");
+    $amp_sym = ansmap_create_from_text($symbols,            "symbol");
+    $amp_fgc = ansmap_create_from_text($foreground_colors,  "foreground");
+    $amp_sty = ansmap_create_from_text($style,              "style");
+
+    $amp = ansmap_create(
+        max(
+            ansmap_get_width($amp_bgc),
+            ansmap_get_width($amp_sym),
+            ansmap_get_width($amp_fgc),
+            ansmap_get_width($amp_sty)
+        ),
+        max(
+            ansmap_get_height($amp_bgc),
+            ansmap_get_height($amp_sym),
+            ansmap_get_height($amp_fgc),
+            ansmap_get_height($amp_sty)
+        )
+    );
+
+    ansmap_draw_sprite($amp, $amp_bgc, 0, 0);
+    ansmap_draw_sprite($amp, $amp_sym, 0, 0);
+    ansmap_draw_sprite($amp, $amp_fgc, 0, 0);
+    ansmap_draw_sprite($amp, $amp_sty, 0, 0);
+
+    return $amp;
+}
+
 function ansmap_to_string(&$amp) {
     $str = "";
+    $h = ansmap_get_height($amp);
+    $w = ansmap_get_width($amp);
 
-    for ($y = 0; $y < count($amp); ++$y) {
-        for ($x = 0; $x < count($amp[$y]); ++$x) {
+    for ($y = 0; $y < $h; ++$y) {
+        for ($x = 0; $x < $w; ++$x) {
             $str .= $amp[$y][$x]["symbol"];
         }
 
@@ -159,7 +287,37 @@ function ansmap_to_string(&$amp) {
     return $str;
 }
 
-$amp_yolo = ansmap_create_from_text("YOLO");
+function ansmap_cell_channel_swap(&$cell, $c1, $c2) {
+    $buf = $cell[$c1];
+    $cell[$c1] = $cell[$c2];
+    $cell[$c2] = $buf;
+}
+
+function ansmap_channel_swap(&$amp, $c1, $c2) {
+    $h = ansmap_get_height($amp);
+    $w = ansmap_get_width($amp);
+
+    for ($y = 0; $y < $h; ++$y) {
+        for ($x = 0; $x < $w; ++$x) {
+            ansmap_cell_channel_swap($amp[$y][$x], $c1, $c2);
+        }
+    }
+}
+
+function ansmap_channel_clear(&$amp, $channel) {
+    $cell = ansmap_cell_create();
+
+    $h = ansmap_get_height($amp);
+    $w = ansmap_get_width($amp);
+
+    for ($y = 0; $y < $h; ++$y) {
+        for ($x = 0; $x < $w; ++$x) {
+            $amp[$y][$x][$channel] = $cell[$channel];
+        }
+    }
+}
+
+$amp_yolo = ansmap_create_from_texts("DDDD", "YOLO", "RGBW");
 
 $amp_screen = ansmap_create(80, 20);
 ansmap_blit($amp_yolo, $amp_screen, 0, 0, 40, 10, 4, 1);
